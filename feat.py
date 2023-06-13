a31683ad1ad827704ee232e05cddb08eed043c32
Ali Heravi <56476428+maheravi@users.noreply.github.com>
2023-06-13 15:09:38 +0330

    Update feat.py

--- feat.py
+++ feat.py
@@ -4,7 +4,7 @@
 import tensorflow as tf
 import keras
 from keras.applications.vgg19 import VGG19
-from keras.preprocessing import image
+from keras.utils import load_img, img_to_array
 from keras.applications.vgg19 import preprocess_input
 import numpy as np
 import matplotlib.pyplot as plt
@@ -15,16 +15,16 @@ from keras import backend as K
 
 #Function to retrieve features from intermediate layers
 def get_activations(model, layer_idx, X_batch):
-    get_activations = K.function([model.layers[0].input, K.learning_phase()], [model.layers[layer_idx].output,])
-    activations = get_activations([X_batch,0])
+    get_activations = K.function([model.layers[0].input], [model.layers[layer_idx].output,])
+    activations = get_activations([X_batch])[0]
     return activations
 
 #Function to extract features from intermediate layers
 def extra_feat(img_path):
         #Using a VGG19 as feature extractor
         base_model = VGG19(weights='imagenet',include_top=False)
-        img = tf.keras.utils.load_img(img_path, target_size=(224, 224))
-        x = tf.keras.utils.img_to_array(img)
+        img = load_img(img_path, target_size=(224, 224))
+        x = img_to_array(img)
         x = np.expand_dims(x, axis=0)
         x = preprocess_input(x)
         block1_pool_features=get_activations(base_model, 3, x)
@@ -33,17 +33,25 @@ def extra_feat(img_path):
         block4_pool_features=get_activations(base_model, 14, x)
         block5_pool_features=get_activations(base_model, 18, x)
 
-        x1 = tf.image.resize_images(block1_pool_features[0],[112,112])
-        x2 = tf.image.resize_images(block2_pool_features[0],[112,112])
-        x3 = tf.image.resize_images(block3_pool_features[0],[112,112])
-        x4 = tf.image.resize_images(block4_pool_features[0],[112,112])
-        x5 = tf.image.resize_images(block5_pool_features[0],[112,112])
+        x1 = tf.image.resize(block1_pool_features[0],[112,112])
+        x2 = tf.image.resize(block2_pool_features[0],[112,112])
+        x3 = tf.image.resize(block3_pool_features[0],[112,112])
+        x4 = tf.image.resize(block4_pool_features[0],[112,112])
+        x5 = tf.image.resize(block5_pool_features[0],[112,112])
+
+        print(x1.shape)
+        print(x2.shape)
+        print(x3.shape)
+        print(x4.shape)
+        print(x5.shape)        
         
-        F = tf.concat([x3,x2,x1,x4,x5],3) #Change to only x1, x1+x2,x1+x2+x3..so on, inorder to visualize features from diffetrrnt blocks
+        # F = tf.concat([x3,x2,x1,x4,x5], axis=3) #Change to only x1, x1+x2,x1+x2+x3..so on, inorder to visualize features from diffetrrnt blocks
+        F = tf.concat([x1,x2,x3,x4,x5], axis=2) #Change to only x1, x1+x2,x1+x2+x3..so on, inorder to visualize features from diffetrrnt blocks
         return F
+
 def main():
   if (len(sys.argv))>3:
-    print("Invalid number of input arguments")
+    print("Invalid number of input arguments ")
     exit(0)
 
   #Two aerial patches with change or No change
@@ -58,9 +66,9 @@ def main():
   F2=tf.square(F2)
   d=tf.subtract(F1,F2)
   d=tf.square(d) 
-  d=tf.reduce_sum(d,axis=3) 
+  d=tf.reduce_sum(d,axis=2) 
 
-  dis=(d.eval())   #The change map formed showing change at each pixels
+  dis=(d.numpy())   #The change map formed showing change at each pixels
   dis=np.resize(dis,[112,112])
 
   # Calculating threshold using Otsu's Segmentation method
